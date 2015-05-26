d37336a43b49c3bf75233443ab654eab1405989d
Søren Vibjerg <vibjerg@gmail.com>
2015-05-26 14:13:54 +0200

    Move examples

--- examples/app.js
+++ examples/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchField from '../components/searchField.react';
+import SearchField from '../src/components/searchField.react';
 
 let element = React.createElement(SearchField);
 React.render(element, document.getElementById('searchfield'));
