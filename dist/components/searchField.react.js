de0263491f16a167fd6c9fbe0d270c0a9adc45ec
Søren Vibjerg <vibjerg@gmail.com>
2015-05-26 14:14:50 +0200

    dist files

--- dist/components/searchField.react.js
+++ dist/components/searchField.react.js
@@ -1,51 +1,58 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
+'use strict';
+Object.defineProperty(exports, '__esModule', {
     value: true
 });
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-var _react = require("react");
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var SearchField = _react2["default"].createClass({
-    displayName: "SearchField",
+require('./searchField.scss');
+
+var SearchField = _react2['default'].createClass({
+    displayName: 'SearchField',
 
     render: function render() {
-        return _react2["default"].createElement(
-            "div",
+        return _react2['default'].createElement(
+            'div',
             null,
-            _react2["default"].createElement(ButtonString, { color: "blue", string: "test" })
+            _react2['default'].createElement(ButtonString, { color: 'blue', string: 'harry' }),
+            _react2['default'].createElement(ButtonString, { color: 'red', string: 'potter' }),
+            _react2['default'].createElement(ButtonString, { color: 'green', string: 'styles' })
         );
     }
 });
 
-var ButtonString = _react2["default"].createClass({
-    displayName: "ButtonString",
+var ButtonString = _react2['default'].createClass({
+    displayName: 'ButtonString',
 
     render: function render() {
         var _props = this.props;
         var color = _props.color;
         var string = _props.string;
 
-        var classes = new Array("buttonstring", color);
-        return _react2["default"].createElement(
-            "div",
-            { className: classes },
-            _react2["default"].createElement(
-                "span",
-                { className: "string" },
+        var classes = new Array('buttonstring');
+        var style = {
+            backgroundColor: color
+        };
+        return _react2['default'].createElement(
+            'div',
+            { className: classes.join(' '), style: style },
+            _react2['default'].createElement(
+                'span',
+                { className: 'text' },
                 string
             ),
-            _react2["default"].createElement(
-                "span",
-                { className: "buttonstring-close" },
-                "x"
+            _react2['default'].createElement(
+                'span',
+                { className: 'remove' },
+                'x'
             )
         );
     }
 });
 
-exports["default"] = SearchField;
-module.exports = exports["default"];
+exports['default'] = SearchField;
+module.exports = exports['default'];
