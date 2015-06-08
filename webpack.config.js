0308e66497f498c0e3413f50024cf8d6b8fb4912
Søren Vibjerg <vibjerg@gmail.com>
2015-06-08 10:09:05 +0200

    remove unused css parsing from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
+'use strict';
+
 /**
  * Config file for webpack
  */
 
 var webpack = require('webpack');
-var path = require('path');
-var extractTextPlugin = require('extract-text-webpack-plugin');
 
 /**
  * Defines the context webpack is running in
@@ -15,15 +15,6 @@ var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
   __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
 });
-
-
-/**
- * Extracts css to a separate file
- *
- * @type {ExtractTextPlugin|exports|module.exports}
- */
-var extractCss = new extractTextPlugin('style.css')
-
 
 /**
  * Setup webpack to transpile ES6 and jsx + handle scss files
@@ -44,19 +35,10 @@ module.exports = {
         test: /\.js?$/,
         loaders: ['babel'],
         exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        loader: extractTextPlugin.extract(
-          // activate source maps via loader query
-          'css?sourceMap!' +
-          'sass?sourceMap'
-        )
       }
     ]
   },
   plugins: [
-    definePlugin,
-    extractCss
+    definePlugin
   ]
 };
