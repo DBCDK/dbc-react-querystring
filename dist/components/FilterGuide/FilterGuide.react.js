91ab51a5c89e591d193255953cd51416bc7fb627
Søren Vibjerg <vibjerg@gmail.com>
2015-06-08 10:14:41 +0200

    setup files for component

---
+++ dist/components/FilterGuide/FilterGuide.react.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
---
+++ src/components/FilterGuide/__tests__/FilterGuide.test.js
@@ -0,0 +1,10 @@
+'use strict';
+//import {expect} from 'chai';
+//import TestUtils from 'react/lib/ReactTestUtils';
+//import React from 'react';
+
+describe('Test the FilterGuide component', () => {
+  it('displays a list of words', ()=> {
+
+  });
+});
