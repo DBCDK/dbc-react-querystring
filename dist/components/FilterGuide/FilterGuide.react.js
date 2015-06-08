9b31e78d424176344e50a462ad66bbc3c111f55f
Søren Vibjerg <vibjerg@gmail.com>
2015-06-08 10:43:21 +0200

    setup basic react structure

--- dist/components/FilterGuide/FilterGuide.react.js
+++ dist/components/FilterGuide/FilterGuide.react.js
@@ -1,4 +1,7 @@
 'use strict';
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
@@ -9,3 +12,63 @@ var _react2 = _interopRequireDefault(_react);
 var _lodash = require('lodash');
 
 var _lodash2 = _interopRequireDefault(_lodash);
+
+/**
+ * Create a List of words for filtering the searchresult
+ */
+var FilterGuide = _react2['default'].createClass({
+  displayName: 'FilterGuide',
+
+  propTypes: {
+    elements: _react2['default'].proptypes.array.isRequired
+  },
+  render: function render() {
+    var elements = this.props.elements;
+
+    return _react2['default'].createElement(
+      'div',
+      { className: 'filterguide' },
+      _react2['default'].createElement(FilterGuideList, { elements: elements })
+    );
+  }
+});
+
+/**
+ * Render an array of words as a scrollable list
+ */
+var FilterGuideList = _react2['default'].createClass({
+  displayName: 'FilterGuideList',
+
+  propTypes: {
+    elements: _react2['default'].proptypes.array.isRequired
+  },
+  render: function render() {
+    var elements = this.props.elements;
+
+    var listItems = elements.map(function (element) {
+      return _react2['default'].createElement(FilterGuideListElement, { element: element });
+    });
+    return _react2['default'].createElement(
+      'div',
+      { className: 'filterguide-list' },
+      listItems
+    );
+  }
+});
+
+var FilterGuideListElement = _react2['default'].createClass({
+  displayName: 'FilterGuideListElement',
+
+  render: function render() {
+    var element = this.props.element;
+
+    return _react2['default'].createElement(
+      'div',
+      { className: 'filterguide-list-element' },
+      element
+    );
+  }
+});
+
+exports['default'] = FilterGuide;
+module.exports = exports['default'];
--- src/components/FilterGuide/FilterGuide.react.js
+++ src/components/FilterGuide/FilterGuide.react.js
@@ -2,3 +2,52 @@
 import React from 'react';
 import _ from 'lodash';
 
+/**
+ * Create a List of words for filtering the searchresult
+ */
+const FilterGuide = React.createClass({
+  propTypes: {
+    elements: React.proptypes.array.isRequired
+  },
+  render() {
+    const {elements} = this.props;
+    return (
+      <div className='filterguide'>
+        <FilterGuideList elements={elements}/>
+      </div>
+    );
+  }
+});
+
+/**
+ * Render an array of words as a scrollable list
+ */
+const FilterGuideList = React.createClass({
+  propTypes: {
+    elements: React.proptypes.array.isRequired
+  },
+  render() {
+    const {elements} = this.props;
+    const listItems = elements.map((element) => (<FilterGuideListElement element={element}/>));
+    return (
+      <div className='filterguide-list'>
+        {listItems}
+      </div>
+    );
+  }
+});
+
+const FilterGuideListElement = React.createClass({
+
+  render() {
+    const {element} = this.props;
+    return (
+      <div className='filterguide-list-element'>
+        {element}
+      </div>
+    );
+  }
+});
+
+
+export default FilterGuide;
