41c0647721230a3ab6e920c63cce3661bda6a9c9
Søren Vibjerg <vibjerg@gmail.com>
2015-06-15 10:54:59 +0200

    remove unused css extract

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -3,15 +3,6 @@
  */
 
 var webpack = require('webpack');
-var extractTextPlugin = require('extract-text-webpack-plugin');
-
-/**
- * Extracts css to a separate file
- *
- * @type {ExtractTextPlugin|exports|module.exports}
- */
-var extractCss = new extractTextPlugin('style.css')
-
 
 /**
  * Setup webpack to transpile ES6 and jsx + handle scss files
@@ -25,18 +16,7 @@ module.exports = {
         test: /\.js?$/,
         loaders: ['babel'],
         exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        loader: extractTextPlugin.extract(
-          // activate source maps via loader query
-          'css?sourceMap!' +
-          'sass?sourceMap'
-        )
       }
     ]
-  },
-  plugins: [
-    extractCss
-  ]
+  }
 };
